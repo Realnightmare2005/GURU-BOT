57a20ca478798708f9f3feb2302127dc678c6e82
Realnightmare2005 <130731211+Realnightmare2005@users.noreply.github.com>
2024-01-22 21:14:42 +0530

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -3,14 +3,10 @@ let handler = async m => m.reply(`
 ≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
 
 ─────────────
-▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
+▢ Join public bot group
 
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
+https://chat.whatsapp.com/FwbSgqroqUGES0NuItfcjN
 
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
 
 ─────────────
 ≡ Disabled links? enter here! 
@@ -18,11 +14,6 @@ https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
 ▢ Group WhatsApp 
  https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
 ─────────────
-▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
-
-▢ *YouTube*
-• https://www.youtube.com/@Asliguru
 
 
 `.trim())
